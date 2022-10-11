f075c3d8f420857de520182dff45bae8f29203de
hymndrazill <srihafiras@yahoo.fr>
2022-10-11 17:08:49 +0100

    state added for the clock component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
+    const [Days,setDays] = useState(10);
+    const [Hours,setHours] = useState(10)
+    const [Minutes,setMinutes] = useState(10)
+    const [Seconds,setSeconds] = useState(10)
+
+  
+
   return (
     <div className="App">
-      Count down app
+        <Clock Days={Days} Hours={Hours} Minutes={Minutes} Seconds={Seconds} />
     </div>
   );
 }
