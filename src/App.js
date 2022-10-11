b7cb14b0165bb76ee85b22a0a9346a6c6ed7c9e6
hymndrazill <srihafiras@yahoo.fr>
2022-10-11 18:01:44 +0100

    Date format works but in a strange way

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Clock from './components/Clock';
 
 function App() {
-    const [Days,setDays] = useState(10);
-    const [Hours,setHours] = useState(10)
-    const [Minutes,setMinutes] = useState(10)
-    const [Seconds,setSeconds] = useState(10)
+    const [Days,setDays] = useState();
+    const [Hours,setHours] = useState()
+    const [Minutes,setMinutes] = useState()
+    const [Seconds,setSeconds] = useState()
 
+    let interval;
   
 
-  return (
+
+
+    const startTimer = () => {
+      const CountDownDate = new Date('December 31, 2022').getTime()
+      console.log(CountDownDate)
+      interval = setInterval(()=>{
+        const now = new Date().getTime();
+        const distance = CountDownDate - now;
+        console.log(distance)
+        const d = Math.floor(distance /(24 *60 *60 *1000));
+        const h = Math.floor(distance % (24 * 60 * 60 *1000) /(60*60*1000));
+        const m = Math.floor(distance % (60 * 60 * 1000) /(60 * 1000));
+        const s = Math.floor(distance % (60 *1000 ) /1000);
+        if (distance < 0) {
+          // stop timer
+          clearInterval(interval.current)
+        } else {
+          // time change
+          setDays(d);
+          setHours(h);
+          setMinutes(m);
+          setSeconds(s);
+        }
+
+      })
+    }
+    useEffect(()=> {
+      startTimer();
+    },[])
+    
+    return (
     <div className="App">
         <Clock Days={Days} Hours={Hours} Minutes={Minutes} Seconds={Seconds} />
     </div>
